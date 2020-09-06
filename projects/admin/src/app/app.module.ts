3c98d04c13a1b75cd7af56988752d03aef538b64
Deepak HB <deepakhb2@gmail.com>
2020-09-06 23:51:34 +0100

    Add authentication for frontent.
    * Use the firebase authentication project for authentication.
    * Refactor the auth status component and moved to cart-firebase project.

--- projects/admin/src/app/app.module.ts
+++ projects/admin/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AngularFireModule } from '@angular/fire';
 import {
   NbThemeModule,
@@ -8,20 +9,17 @@ import {
   NbButtonModule
 } from '@nebular/theme';
 
-import { environment } from './../environments/environment';
-
 import { FirebaseModule } from 'cart-firebase';
+
+import { environment } from './../environments/environment';
 import { CartModule } from './cart/cart.module';
 import { AuthGuardService } from './services/auth-guard.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthStateComponent } from './components/auth-state/auth-state.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthStateComponent,
   ],
   imports: [
     BrowserModule,
