ae2f98fc0c4f7b9aad88fdb01ca308011d1c40a2
Deepak HB <deepakhb2@gmail.com>
2020-09-10 19:26:38 +0100

    Add username and change button after login and logout.

--- projects/cart-firebase/src/lib/firebase/components/auth-state/auth-state.component.ts
+++ projects/cart-firebase/src/lib/firebase/components/auth-state/auth-state.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { catchError, share, take } from 'rxjs/operators';
 import { NbAuthService, NbAuthToken } from '@nebular/auth';
+import { selectShoppingCurrentUser } from '../../state/selectors'
 
 @Component({
   selector: 'app-auth-state',
@@ -11,8 +13,10 @@ import { NbAuthService, NbAuthToken } from '@nebular/auth';
 })
 export class AuthStateComponent implements OnInit {
   isAuthenticated$: Observable<boolean>;
+  user$: Observable<any>;
 
   constructor(
+    private store: Store<{ items: number }>,
     private authService: NbAuthService,
     private router: Router,
     private route: ActivatedRoute,
@@ -20,7 +24,9 @@ export class AuthStateComponent implements OnInit {
     this.isAuthenticated$ = this.authService.isAuthenticated();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user$ = this.store.select(selectShoppingCurrentUser)
+  }
 
   logout() {
     this.router.navigate(['/auth/logout']);
